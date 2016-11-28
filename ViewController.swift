9b951d3d05897f1c442415d8f8cef271190fcbad
Muhammad Rizwan <muhammad.rizwan904u@gmail.com>
2016-11-28 18:02:13 +0500

    NextGeni
    login with MVC only

---
+++ ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  GmailTestApp
+//
+//  Created by Rizwan Mughal on 28/11/2016.
+//  Copyright © 2016 Rizwan Mughal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func loginButton(_ sender: AnyObject) {
+        
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Sum THAT IS FIRST OR SECOND")
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
